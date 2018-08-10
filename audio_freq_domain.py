ef8b887bdc58c6c10da97b7029e35db8780dd995
psvkushal <39593206+psvkushal@users.noreply.github.com>
2018-08-10 22:00:18 +0530

    Update audio_freq_domain.py

--- audio_freq_domain.py
+++ audio_freq_domain.py
@@ -4,25 +4,25 @@ import matplotlib.pyplot as plt
 from scipy.fftpack import fft, fftfreq, fftshift
 
 
-rate ,data = wav.read('file_example_WAV_1MG.wav')    #reading the data using scipy.io.wavfile module
+rate ,data = wav.read('1kHz_44100Hz_16bit_05sec.wav')    #reading the data using scipy.io.wavfile module
 nframe = data.shape[0] 		#no of frames in the audio file
-x = np.arange(nframe)
-T = 1/rate
-#print(data)				#time diff between two samples
-xf = fftfreq(nframe, T)     
-print(xf)
-yf = fft(data)
-yf = np.abs(yf)     #from data it is removing imaginary part of the data
-print(yf)
-plt.plot(xf,yf)
-plt.show()
-
-
-#incase of wav file having two channels use this for plotting
-'''
-plt.subplot(211)
-plt.plot(xf,yf[:,0])
-plt.subplot(212)
-plt.plot(xf,yf[:,1])
-plt.show()
-'''
+print(data)
+T = 1/rate	#time diff between two samples
+dt = 0.005
+nsamp = int((dt)/T)
+print(nsamp)
+for i in np.arange(0,nframe,nsamp):	
+	d = data[i:(i+nsamp)]	
+	plt.hold(True)
+	plt.clf()
+	plt.subplot(211)
+	plt.plot(d)
+	f = fftfreq(nsamp, T)     
+	print(f)
+	Y = fft(d)
+	Y = np.abs(Y)     #from data it is removing imaginary part of the data
+	print(Y)
+	plt.subplot(212)
+	plt.plot(f,Y)
+	plt.pause(0.01)
+	plt.hold(False)
